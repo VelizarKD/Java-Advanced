ea21ffed57f569aeb1fe67ef17b59a21e5e8a8cf
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2023-05-02 16:19:01 +0300

    main

--- src/Python/PythonGame.java
+++ src/Python/PythonGame.java
@@ -11,10 +11,12 @@ public class PythonGame {
         char[][] matrix = new char[n][n];
         // fill матрицата
         for (int row = 0; row < n; row++) {
-            matrix[row] = scanner.nextLine().toCharArray();
+            matrix[row] = scanner.nextLine().replaceAll(" ", "").toCharArray();
         }
 
         //find Python(snake)
+        //find all food
+        int countFood = 0;
         int rowPython = 0;
         int colPython = 0;
         for (int row = 0; row < n; row++) {
@@ -23,24 +25,74 @@ public class PythonGame {
                 if (currentElement == 's') {
                     rowPython = row;
                     colPython = col;
+                } else if (currentElement == 'f') {
+                    countFood++;
                 }
             }
         }
         //moves
         int length = 1;
+        boolean isDead = false;
         for (String command : commands) {
+            matrix[rowPython][colPython] = '*';
             //left/right/up/down
             switch (command) {
                 case "left":
                     colPython--;
                     break;
                 case "right":
+                    colPython++;
                     break;
                 case "up":
+                    rowPython--;
                     break;
                 case "down":
+                    rowPython++;
                     break;
             }
+
+            if (rowPython < 0 || rowPython >= n) {
+                if (rowPython < 0) {
+                    rowPython = n - 1;
+                }
+
+                if (rowPython >= n) {
+                    rowPython = 0;
+                }
+            }
+
+            if (colPython < 0 || colPython >= n) {
+                if (colPython < 0) {
+                    colPython = n - 1;
+                }
+                if (colPython >= n) {
+                    colPython = 0;
+                }
+            }
+
+            if (countFood == 0) {
+                break;
+            }
+            //check where is the snake
+            if (matrix[rowPython][colPython] == 'f') {
+                length++;
+                countFood--;
+            }
+
+            if (matrix[rowPython][colPython] == 'e') {
+
+                isDead = true;
+                break;
+            }
+            matrix[rowPython][colPython] = 's';
         }
+        if (countFood == 0) {
+            System.out.println("You win! Final python length is " + length);
+        } else if (isDead) {
+            System.out.println("You lose! Killed by an enemy!");
+        } else {
+            System.out.printf("You lose! There is still %d food to be eaten.", countFood);
+        }
+
     }
 }
